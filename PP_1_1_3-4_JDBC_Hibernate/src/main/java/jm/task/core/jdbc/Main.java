0e013713e4c1e0226a2fff1b3c7139adbdfc0e79
AlexandrStark0v <118987001+AlexandrStark0v@users.noreply.github.com>
2025-05-03 23:50:40 +0300

    Update Main.java
    Создание 4 юсеров

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,24 +1,30 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-//        Connection connection = Util.getConnection();
-//
-//        if(connection != null) {
-//            System.out.println("Подключились");
-//        }else {
-//            System.out.println("Нет");
-//        }
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
-        userDao.saveUser("Test", "User", (byte) 25);
-        System.out.println(userDao.getAllUsers());
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Саня", "Старков", (byte) 25);
+        userService.saveUser("Татьяна", "Косолапова", (byte) 20);
+        userService.saveUser("Игорь", "Подорожников", (byte) 31);
+        userService.saveUser("Артур", "Пирожков", (byte) 50);
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
