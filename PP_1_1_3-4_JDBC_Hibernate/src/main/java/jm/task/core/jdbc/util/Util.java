a2ac4ec273096e1f578dd42d3136205a5d082da6
AlexandrStark0v <118987001+AlexandrStark0v@users.noreply.github.com>
2025-05-06 23:41:46 +0300

    Подключение hibernate

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,24 +1,55 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private  static  final String URL = "jdbc:mysql://localhost:3306/alex_star";
-    private  static  final String USERNAME = "root";
-    private  static  final String PASSWORD = "root";
+    private static SessionFactory sessionFactory;
+    private static final String URL = "jdbc:mysql://localhost:3306/alex_star";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() {
         Connection conn = null;
         try {
-          conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             System.out.println("Соединение есть!");
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Нет соединения!");
             e.printStackTrace();
         }
         return conn;
+    }
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.username", USERNAME);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.setProperty("hibernate.hbm2ddl.auto", "none");
+
+            configuration.addAnnotatedClass(User.class);
+
+            sessionFactory = configuration.buildSessionFactory();
+
+            System.out.println("SF создан");
+        } catch (Exception ex) {
+            System.out.println("Ошибка создания SF: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
