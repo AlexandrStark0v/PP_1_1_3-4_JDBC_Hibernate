c567aa8af643240acdc58e8337887828b4dd8be4
AlexandrStark0v <118987001+AlexandrStark0v@users.noreply.github.com>
2025-05-08 22:27:09 +0300

    Исправил

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -9,8 +10,9 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
+
     public UserServiceImpl() {
-        this.userDao = new UserDaoJDBCImpl();
+        this.userDao = new UserDaoHibernateImpl();
     }
 
     public void createUsersTable() {
